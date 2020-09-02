42f56dde1f7b91f844dde3c55b4b079f3f62a51b
Carlos Ixtetela <carlos.ixtetela@gmail.com>
2020-09-01 23:11:33 -0600

    rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { FormComponent } from './pages/form/form.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent,
+    FormComponent,
+    /* ReactiveFormsModule, */
+
   ],
   imports: [
     BrowserModule
